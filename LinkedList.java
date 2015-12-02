578fcbbb50a4f2e40aa613a5f9e51371f53b3b01
acooper2016 <acooper2016@pingry.org>
2015-12-01 22:55:16 -0500

    Final commit of LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,6 @@
+import java.util.*;
 
-
-public class LinkedList<E>
+public class LinkedList<E> implements Iterable, Stack, Queue
 {
 	private ListNode<E> head;
 	private ListNode<E> tail;
@@ -16,6 +16,19 @@ public class LinkedList<E>
 		size = 0;
 	}
 	
+	public LinkedList(LinkedList<E> list)
+	{
+		size = 0;
+		if(list.size() > 0)
+		{
+			for(int i = 0; i < list.size(); i++)
+			{
+				this.add(list.get(i));
+			}
+		}
+	}
+	
+	
 	/**
 	*Returns number of nodes in the Linkedlist
 	*
@@ -26,6 +39,7 @@ public class LinkedList<E>
 		return size;	
 	}
 	
+	
 	/**
 	*Adds a new node containing item to the end of the LinkedList
 	*
@@ -34,76 +48,461 @@ public class LinkedList<E>
 	public boolean add(E item)
 	{
 		ListNode<E> addend = new ListNode<E>(item);
-		if(head == null)	//If LinkedList is empty, just creates new node containing item and sets it to head and tail.
+		if(size == 0)
 		{
-			head = addend;
 			tail = addend;
+			head = addend;
 		}
 		else
-			this.add(addend, head);	//Passes new ListNode to first node 
+		{
+			tail.setNext(addend);
 			tail = addend;
-		size ++;
+		}
+		size++;
 		return true;
 	}
 	
-	private void add(ListNode<E> addend, ListNode<E> location)
-	{
-		if(location.getNext() == null)	//If location is at end of LinkedList, sets its pointer to addend
-			location.setNext(addend);
-		else
-			add(addend, location.getNext());//Passes addend to location's pointer.
-	}
 	
+	/**
+	*Adds a specified item at a given index.
+	*
+	*@param index index where item is to be added
+	*@param item item to be added
+	*
+	*@return whether or not item was successfully added.
+	*/
 	public boolean add(int index, E item)
 	{
 		
-		if(index < 0)
-			throw new IndexOutOfBoundsException("index must be > -1");	
+		if(index < 0 || index > size )
+			throw new IndexOutOfBoundsException();	
 		ListNode<E> n = new ListNode<E>(item);
-		if(index == 0 && head == null)	//If LinkedList is empty and n is to be added at index 0, sets head to n.
-			head = n;
-		else if(index == 0)	//If LinkedList is not empty is to be added at index 0, inserts n into pointer chain at head
+		if(index == 0)
 		{
 			n.setNext(head);
 			head = n;
 		}
-		else if(head == null)	//if LinkedList is empty but n is to be added at a value higher than 0, creates new empty ListNode for head and passes n to it.
+		else if(index == size)
 		{
-			head = new ListNode<E>(null);
+			return add(item);
+		}
+		else	
+		{
 			add(index - 1, n, head);
 		}
-		else	//Passes n to ListNode located at head pointer.
-		{
-			add(index - 1, n, head);
-		}
-		if(index == size)	//If new ListNode is inserted at the end, sets tail to it.
-			tail = n;
 		size ++;
 		return true;
 	}
+	/*
+		Algorithm is pretty simple recursion.  If adding to the first or last index, it will just use head or tail to figure
+		out what what pointer to insert to.  Other than that, it will use a helper method that takes in the location and the 
+		current index that decreases with each recursion.  This current index allows the method to insert at an index given by the user.
+		Base case occurs when the index passed is 0, and will insert at that point in the list.  
+	*/
 	
+	/* Helper method for add at index.*/
 	private void add(int index, ListNode<E> n, ListNode<E> location)
 	{
-		if(index == 0)	//Base Case.  Inserts n after location.
+		if(index == 0)	
 		{
 			n.setNext(location.getNext());
 			location.setNext(n);
 		}
-		else if(location.getNext() == null)	//Creates new empty ListNode after location, and passes n to it.
-		{
-			location.setNext(new ListNode<E>(null));
-			add(index - 1, n, location.getNext());
-		} 
-		else	//passes n to location's pointer.
+		else	
 			add(index - 1, n, location.getNext());
 	}
 	
+	
+	
+	/**
+	*Returns value at a given index.
+	*
+	*@param index index of the item to be returned.
+	*@return item at given index
+	*/
+	public E get(int index)
+	{
+		if(index > size - 1 || index < 0)
+			throw new IndexOutOfBoundsException();
+		else if(index == 0)
+			return head.getItem();
+		else if(index == size - 1)
+			return tail.getItem();
+		else
+			return get(index - 1, head.getNext());
+	}
+	
+	public E get(int index, ListNode<E> location)
+	{
+		if(index == 0)
+			return location.getItem();
+		return get(index - 1, location.getNext());
+	}
+	/*
+	Algorithm is as follows.  If index equates to head or tail, will just return those items.  Otherwise, will pass to a recursive
+	Helper method that counts # of recursions with an index that decreases with each pass.  If index isn't zero, will pass to next
+	ListNode.  Once index reaches 0, base case will trigger indicating that this ListNode contains the desired item.  Returns that
+	item.
+	*/
+	
+	/**
+	*Removes first instance of a given item.  
+	*
+	*@param o Item to be removed.
+	*@return whether LinkList contained any instances of the given item to be removed.
+	*/
+	public boolean remove(E o)
+	{
+		if(size() == 0)
+			return false;
+		else if(head.getItem() == o)
+		{
+			ListNode<E> holder = head;
+			head = head.getNext();
+			holder.setNext(null);
+			size --;
+			return true;
+		}	
+		else
+			return remove(o, head.getNext());
+			
+	}
+	/*
+	Algorithm is as follows:  If head matches o, sets head to following ListNode
+	and dereferences previous head.  If not, passes to recursive helper function.
+	Helper function recurs through each ListNode, checking if following ListNode's
+	item = o.  If it does, Links that ListNode to the ListNode 2 links away, dereferences
+	the ListNode 1 link away, and returns true.  If it passes to the final ListNode,
+	returns false.
+	*/
+	
+	/*Helper method of recursive remove(E) method*/
+	private boolean remove(E o, ListNode<E> node)
+	{
+		if(node.getNext() == null)
+			return false;
+		else if(node.getNext().getItem() != o)
+			return remove(o, node.getNext());
+		else
+		{
+			if(node.getNext().getNext() == null)
+			{
+				tail = node;
+				node.setNext(null);
+			}
+			else
+			{	
+				ListNode<E> holder = node.getNext();
+				node.setNext(node.getNext().getNext());
+				holder.setNext(null);
+			}
+			size --;
+			return true;
+		}	
+	}
+	/**
+	*Removes the item at a given index.
+	*
+	*@param index index of item to be removed
+	*@return removed item
+	*/
+	public E remove(int index)
+	{
+		if(index >= size || index < 0)
+			throw new IndexOutOfBoundsException();
+		else if(index == 0)
+		{
+			ListNode<E> holder = head;
+			head = head.getNext();
+			holder.setNext(null);
+			size --;
+			return holder.getItem();
+		}
+		else
+		{
+			return remove(index -1, head);
+		}
+	}
+	/*
+	Algorithm is as follows:  If index = 0, sets head to following ListNode
+	and dereferences previous head.  If not, passes to recursive helper function.
+	Helper function recurs through each ListNode, reducing a counter parameter by 1.
+	When the function passes to a ListNode and the counter hits 0, Links that ListNode 
+	to the ListNode 2 links away, dereferences the ListNode 1 link away, and returns the contents
+	of the dereferenced ListNode.  If it passes to the final ListNode, returns false.
+	*/
+	
+	/*Helper recursive method of remove(int)*/
+	private E remove(int index, ListNode<E> node)
+	{
+		if(index == 0)
+		{
+			if(node.getNext().getNext() == null)
+			{
+				tail = node;
+				ListNode<E> holder = node.getNext();
+				node.setNext(null);
+				size --;
+				return holder.getItem();
+			}
+			else
+			{
+				ListNode<E> holder = node.getNext();
+				node.setNext(node.getNext().getNext());
+				holder.setNext(null);
+				size --;
+				return holder.getItem();
+			}
+		}
+		else
+		{
+			return remove(index - 1, node.getNext());
+		}	
+	}
+	
+	/**
+	*Adds item to the front of the Stack
+	*
+	*@param item item to be added
+	*/
+	public void push(Object item)
+	{
+		add(0, (E)item);
+	}
+	/**
+	*Adds item to the front of the Queue
+	*
+	*@param item item to be added
+	*/
+	public void offer(Object item)
+	{
+		add(0, (E)item);
+	}
+	
+	/**
+	*Removes first item in LinkedList
+	*
+	*@return removed item
+	*/
+	public E removeFirst()
+	{
+		if(size() == 0)
+			throw new NoSuchElementException();
+		return remove(0);
+	}
+	
+	/**
+	*Removes last item in LinkedList
+	*
+	*@return removed item
+	*/
+	public E removeLast()
+	{
+		if(size() == 0)
+			throw new NoSuchElementException();
+		return remove(size - 1);
+	}
+	
+	/**
+	*Adds an item as the first entry in the LinkedList.
+	*
+	*@param item item to be added
+	*/
+	public void addFirst(E item)
+	{
+		add(0, item);
+	}
+	
+	/**
+	*Adds an item as the last entry in The linkedList.
+	*
+	*@param item item to be added
+	*/
+	public void addLast(E item)
+	{
+		add(item);
+	}
+	
+	/**
+	*Checks whether a given item is in LinkedList
+	*
+	*@param o item being checked for
+	*@return whether or not LinkedList contains o
+	*/
+	public boolean contains(E o)
+	{
+		if(size == 0)
+			return false;
+		else if(head.getItem() == o)
+			return true;
+		else 
+			return contains(o, head.getNext());		
+	}
+	
+	/*Recursive helper method for Contains(E)*/
+	private boolean contains(E o, ListNode<E> node)
+	{
+		if(node.getItem() == o)
+			return true;
+		else if(node.getNext() == null)
+			return false;
+		else
+			return contains(o, node.getNext());
+	}
+	
+	/**
+	*Removes all data in the LinkedList
+	*/
+	public void clear()
+	{
+		size = 0;
+		head = null;
+		tail = null;
+	}
+	
+	/**
+	*Sets the ListNode at a given Index to a new item
+	*
+	*@param index index where new item is to be set
+	*@param o item that is to replace original item at index
+	*@return Previous item held at index.
+	*/
+	public E set(int index, E o)
+	{
+		if(index >= size || index < 0)
+			throw new IndexOutOfBoundsException();
+		else if(index == 0)
+		{
+			E holder = head.getItem();
+			head.setItem(o);
+			return holder;
+		}
+		else
+		{
+			return set(index - 1, o, head.getNext());
+		}
+	}
+	
+	private E set(int index, E o, ListNode<E> node)
+	{
+		if(index == 0)
+		{
+			E holder = node.getItem();
+			node.setItem(o);
+			return holder;
+		}
+		else
+		{
+			return set(index - 1, o, node.getNext());
+		}
+	}	
+	/*
+	Algorithm is largely a simplified version of Remove(index).  Rather than dereferencing
+	the ListNode at index, simply sets its item to a new item.
+	*/
+	
+	/**
+	*Returns index of a given item.  
+	*
+	*@param o item being searched for.
+	*@return index of o.  -1 if LinkedList does not contain o
+	*/
+	public int indexOf(E o)
+	{
+		if(size == 0)
+			return -1;
+		else if(head.getItem() == o)
+			return 0;
+		else
+			return indexOf(o, 1, head.getNext()); 
+	}
+	
+	private int indexOf(E o, int index, ListNode<E> node)
+	{
+		if(node.getItem() == o)
+			return index;
+		else if(node.getNext() == null)
+			return -1;
+		else
+			return indexOf(o, index + 1, node.getNext());
+	}
+	/*
+	Algorithm is as follows:  If head's item is o, returns 0.  Else, passes to a recursive
+	helper function, along with a counter parameter that increases by one with each 
+	pass.  If the object is found, returns current value of counter parameter.  If
+	end of LinkedList is reached, returns -1.
+	*/
+	
+	/**
+	*Removes first entered item in queue.
+	*
+	*@return first entered item in queue.
+	*/
+	public E poll()
+	{
+		return removeLast();
+	}
+	
+	/**
+	*Removes last item entered in stack.
+	*
+	*@return last item entered in stack
+	*/
+	public E pop()
+	{
+		return removeFirst();
+	}
+	
+	/**
+	*Returns first entered item in Queue.
+	*
+	*@return first entered item in Queue.
+	*/
+	public E peak()
+	{
+		return tail.getItem();
+	}
+	
+	/**
+	*Returns last entered item in Stack.
+	*
+	*@return last entered item in Stack.
+	*/
+	public E peek()
+	{
+		return head.getItem();
+	}	
+	
+	/**
+	*Returns whether or not LinkedList is empty.
+	*
+	*@return whether or not LinkedList is empty.
+	*/
+	public boolean isEmpty()
+	{
+		if(size() == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	*Returns string representation of the LinkedList.
+	*
+	*@return String representation of the linkedlist.
+	*/
 	public String toString()
 	{
 		if(head == null)
 			return "";
-		return head.toString();
+		return head.getString();
 	}
+	
+	public Iterator<E> iterator()
+	{
+		return new LinkedListIterator(head);
+	}
+	
 /*
 *	PLACE
 *	HOLDER
@@ -156,12 +555,50 @@ private class ListNode<E>
 	
 	public String toString()
 	{
+		return (String) item;
+	}
+	
+	/*
+	Recursive helper method of LinkedList.toString().  Each recursion appends the next ListNode to the original string, until
+	end of chain is reached.
+	*/
+	public String getString()
+	{
 		if(getNext() == null)
 			return "\n" + item.toString();
-		return "\n" + item + getNext().toString();
+		return "\n" + item + getNext().getString();
 	}
 	
 	
 }
+
+public class LinkedListIterator implements Iterator
+{
+	private ListNode<E> curr;
+	
+	public LinkedListIterator(ListNode<E> head)
+	{
+		curr = head;
+	}
+
+	public boolean hasNext()
+	{
+		return (curr.getNext() == null);
+	}
+	
+	public E next()
+	{
+		if(curr.getNext() == null)
+			throw new IndexOutOfBoundsException();
+		E item = curr.getItem();
+		curr = curr.getNext();
+		return item;
+	}
+	
+	public void remove()
+	{
+	}
+}
+
 }
 
